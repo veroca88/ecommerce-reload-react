c36474fdc99508ea3d9a490e6beb39ed2d1d2c1c
veroca88 <veronicadonoso1506@gmail.com>
2020-05-25 21:13:36 -0400

    Use context and call methods

--- src/components/SingleProduct/SingleProduct.js
+++ src/components/SingleProduct/SingleProduct.js
@@ -1,47 +1,61 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import { ProductContext } from "../context/ProductsProvider";
+
+// List of all the products
+
 class SingleProduct extends Component {
+  
   render() {
     const { _id, name, cost, image, inShoppingCart } = this.props.eachProduct;
     return (
-      <div className="col-9 mx-auto col-md-6 col-lg-3 my-3">
-        <div className="card">
-          <div
-            onClick={console.log("You clicked it")}
-            className="img-container"
-          >
-              <Link to={{
-                pathname: `/details/${_id}`,
-                state: {
-                    props: this.props.eachProduct
-                }
-            }}>
-              <img className="imageHeigth card-img" src={image} alt={name} />
-            </Link>
-            <div className="text-card">
-              <p>{name}</p>
-              <p>${cost}</p>
-            </div>
+      <ProductContext.Consumer>
+        {context => (
+            <div className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+              <div className="card">
+                <div className="img-container"
+                onClick={() => context.getProductById(_id)}
+                >
+                  <Link
+                    to={{
+                      pathname: `/details/${_id}`
+                    }}
+                  >
+                    <img
+                      className="imageHeigth card-img"
+                      src={image}
+                      alt={name}
+                    />
+                  </Link>
+                  <div className="text-card">
+                    <p>{name}</p>
+                    <p>${cost}</p>
+                  </div>
 
-            <button
-              className="cart-btn"
-              disabled={inShoppingCart ? true : false}
-              onClick={() => {
-                console.log("add to the cart...");
-              }}
-            >
-              {inShoppingCart ? (
-                <i className="fas fa-cart-arrow-down" disabled></i>
-              ) : (
-                <i className="fas fa-tag"></i>
-              )}
-            </button>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
+                  <button
+                    className="cart-btn"
+                    disabled={inShoppingCart ? true : false}
+                    onClick={() => {
+                      context.addToCart(_id); 
+                      // context.knobOpen(_id);
+                    }}>
+                    {inShoppingCart ? (
+                      <i className="fas fa-cart-arrow-down" disabled></i>
+                    ) : (
+                      <i className="fas fa-tag"></i>
+                    )}
+                  </button>
+                </div>
+              </div>
+            </div>
+          // );
+        // }
+        )
+        }
+      </ProductContext.Consumer>
+        );
+      }
+    }
 
 export default SingleProduct;
