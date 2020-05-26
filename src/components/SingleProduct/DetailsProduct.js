c25979652a3e933d4e881d1a10108684387a5117
veroca88 <veronicadonoso1506@gmail.com>
2020-05-25 21:15:14 -0400

    Adjust method to save order item in shopping cart

--- src/components/SingleProduct/DetailsProduct.js
+++ src/components/SingleProduct/DetailsProduct.js
@@ -2,91 +2,109 @@ import React, { Component } from "react";
 import { ProductContext } from "../context/ProductsProvider";
 import { Link } from "react-router-dom";
 
-class DetailsProduct extends Component {
-  state = {
-    currentProduct: this.props.location.state.props,
-  }
+// Product description
 
-  // componentDidMount() {
-  //   console.log('---------------', this.props.location.state.props)
-  //   const product = this.props.location?.state?.props
-  //   ? this.props.location.state.props
-  //   : this.state.currentProduct;
-  //   this.setState({
-  //     currentProduct: product
-  //   })
-  // }
+class DetailsProduct extends Component {  
   render() {
-    console.log('PRODUCT', this.state.currentProduct)
-    const { currentProduct } = this.state
+    const { productId } = this.props.match.params;
     return (
       <>
-      <section>
-        <form onSubmit={console.log('Submited DEtails')} >
-          <div className="detail-card">
-            <div className="row ">
-              <div className="col-md-8">
-                <img alt={currentProduct.name} src={currentProduct.image} className="w-100" />
-              </div>
-              <div className="col-md-4 product-info">
-                <h4 className="product-title">{currentProduct.name}</h4>
-                <hr />
-                <p>
-                  Price: <strong className="cost-box">${currentProduct.cost}</strong>
-                </p>
-                <p>
-                  Material :<strong className="material-box">{currentProduct.material}</strong>
-                </p>
-                <div className="size-box">
-                  <select name="size" onChange={this.handleItem}>
-                    <option>Select size</option>
-                    {currentProduct.size &&
-                      currentProduct.size.map((eachSize, index) => {
-                        return (
-                          <option key={index} value={eachSize}>
-                            {eachSize}
-                          </option>
-                        );
-                      })}
-                  </select>
-                </div>
+        <ProductContext.Consumer>
+          {context => {
+            const { currentProduct, orderItem } = context.state;
+            const { handleItem, addToCart, handleSubmit } = context
+            return (
+              // <>
+              // hi
+              // </>
+                           <section>
+                <form onSubmit={handleSubmit}>
+                  <div className="image-box">
+                    <div className="row ">
+                      <div className="col-md-8">
+                        <img
+                          alt={orderItem.name}
+                          src={orderItem.image}
+                          className="w-100"
+                        />
+                      </div>
+                      <div className="col-md-4 product-info">
+                        <h4 className="product-title">{orderItem.name}</h4>
+                        <hr />
+                        <p>
+                          Price:{" "}
+                          <strong className="cost-box">
+                            ${orderItem.cost}
+                          </strong>
+                        </p>
+                        <p>
+                          Material :
+                          <strong className="material-box">
+                            {orderItem.material}
+                          </strong>
+                        </p>
+                        <div className="size-box">
+                          <select name="size" 
+                          onChange={handleItem}
+                          >
+                            <option>Select size</option>
+                            {currentProduct.size &&
+                              currentProduct.size.map((eachSize, index) => {
+                                return (
+                                  <option key={index} value={eachSize}>
+                                    {eachSize}
+                                  </option>
+                                );
+                              })}
+                          </select>
+                        </div>
 
-                <div className="size-box">
-                  <select name="color" onChange={this.handleItem}>
-                    <option>Select color</option>
-                    {currentProduct.color &&
-                      currentProduct.color.map((eachColor, index) => {
-                        return (
-                          <option key={index} value={eachColor}>
-                            {eachColor}
-                          </option>
-                        );
-                      })}
-                  </select>
-                </div>
-                <div 
-                className="product-price-btn"
-                // className="box-btns"
-                >
+                        <div className="size-box">
+                          <select name="color" 
+                          onChange={handleItem}
+                          >
+                            <option>Select color</option>
+                            {currentProduct.color &&
+                              currentProduct.color.map((eachColor, index) => {
+                                return (
+                                  <option key={index} value={eachColor}>
+                                    {eachColor}
+                                  </option>
+                                );
+                              })}
+                          </select>
+                        </div>
+                        <div
+                        // className="product-price-btn"
+                        // className="box-btns"
+                        >
+                          <button
+                            className="product-price-btn"
+                            disabled={orderItem.inShoppingCart ? true : false}
+                            onClick={() => {addToCart(orderItem)}}
+                          >
+                            {orderItem.inShoppingCart ? (
+                              <h6 disabled>In Cart</h6>
+                            ) : (
+                              <h6>Add to Cart</h6>
+                            )}
+                          </button>
 
-                
-                {/* <button
-                  // className="product-price-btn"
-                  disabled={isInShopBag ? true : false}
-                  onClick={this.handleItemInBag}
-                >
-                  {isInShopBag ? <h6 disabled>In Bag</h6> : <h6>Add </h6>}
-                </button> */}
-
-                
-                </div>
-              </div>
-            </div>
-          </div>
-        </form>
-      </section>
+                          <Link to="/products">
+                            <button className="product-price-btn">
+                              <h6>Back to Products</h6>
+                            </button>
+                          </Link>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </section>
+            )
+          }}
+        </ProductContext.Consumer>
       </>
-      
     );
   }
 }
